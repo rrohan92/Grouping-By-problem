bc987c9880acf7d8f79f2815f743c1d6b4246832
Rohan Doshi <rrohan92@gmail.com>
2016-06-28 18:32:07 -0400

    Final version

--- src/Main.java
+++ src/Main.java
@@ -2,80 +2,94 @@ import java.util.*;
 
 public class Main {
 
-	public void grouping(int [][] matrix){
+	public int[][] groupPaysEval(int [][] matrix){
+		//Check if matrix is null or empty
+		if(matrix == null || matrix.length == 0){
+			return new int[][]{};
+		}
+
+		//Declarations
 		int numRows = matrix.length;
 		int numCols = matrix[0].length;
 		int [][] groupSizes = null;
-		boolean [][] set = new boolean[matrix.length][matrix[0].length];
+		boolean [][] set = new boolean[numRows][numCols];
+		List<int []> groups = new ArrayList<int []>();
 
+
+		//Initializing the boolean array with true
 		for(boolean[] row: set){
 			Arrays.fill(row, true);
 		}
 
-		List<int []> groups = new ArrayList<int []>();
-
-		for(int i=0; i<matrix.length; i++){
-			for(int j=0; j<matrix[0].length; j++){
+		for(int i=0; i<numRows; i++){
+			for(int j=0; j<numCols; j++){
 				if(set[i][j]){
 					int count = 0;
 					int finalCount = createGroups(matrix, set, i, j, count, numRows, numCols);
 
+					//System.out.println(matrix[i][j] + " " + finalCount);
 					//Insert into list
-					System.out.println(matrix[i][j] + " " + finalCount);
+					groups.add(new int[]{matrix[i][j], finalCount});
 				}
 			}
 		}
-		//return groupSizes;
+		groupSizes = groups.toArray(new int[groups.size()][]);
+		return groupSizes;
 	}
-	
-	public int getval(int [][] matrix, int row, int column, int numRows, int numCols){
-	
+
+	public int getValue(int [][] matrix, int row, int column, int numRows, int numCols){
+
 		if(row >= 0 && row < numRows && column >= 0 && column < numCols){
 			return matrix[row][column];
 		}
-		
+
 		else{
 			return Integer.MIN_VALUE;
 		}
 	}
 
 	public int createGroups(int [][] matrix, boolean [][] set, int row, int column, int count, int numRows, int numCols){
-		
+
 		if(row >= numRows || column >= numCols){
 			return Integer.MIN_VALUE;
 		}
-		
+
 		count++;
 		set[row][column] = false;
 		int direction [][] = {{1,0}, {0,1}, {-1,0}, {0,-1}};
-		
+
 		for(int i=0; i<4; i++){
 			int newi = row + direction[i][0];
 			int newj = column + direction[i][1];
-			
-			int val = getval(matrix, newi, newj, numRows, numCols);
-			
-			if(val == matrix[row][column] && set[newi][newj]){
-				//System.out.println(newi+" "+newj);
-				count=createGroups(matrix, set, newi, newj,count, numRows, numCols);
+
+			int value = getValue(matrix, newi, newj, numRows, numCols);
+
+			if(value == matrix[row][column] && set[newi][newj]){
+				count = createGroups(matrix, set, newi, newj,count, numRows, numCols);
 			}
 		}
-		
 		return count;
 	}
-	
+
 	public static void main(String[] args) {
 
 		Main obj = new Main();
 
 		int [][] matrix = new int[][]{
-				{0,0,0,0},
-				{0,0,1,0},
-				{0,0,0,1},
-				{0,0,0,0}
+				{0,1,1,3},
+				{1,0,1,2},
+				{1,0,0,2},
+				{1,1,1,1}
 		};
-		
-		obj.grouping(matrix);
-	}
 
+		int [][] groupSizes = obj.groupPaysEval(matrix);
+
+		//Display results
+		for(int i=0; i<groupSizes.length; i++){
+			for(int j=0; j<groupSizes[0].length; j++){
+				System.out.print(groupSizes[i][j] + " ");
+			}
+			System.out.println();
+		}
+	}
 }
