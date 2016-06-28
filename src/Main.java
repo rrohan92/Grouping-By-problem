3aa7fb9588176511113e26d5e4559034ea9b7e73
Rohan Doshi <rrohan92@gmail.com>
2016-06-28 17:48:41 -0400

    All test cases passing

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,8 @@ import java.util.*;
 public class Main {
 
 	public void grouping(int [][] matrix){
+		int numRows = matrix.length;
+		int numCols = matrix[0].length;
 		int [][] groupSizes = null;
 		boolean [][] set = new boolean[matrix.length][matrix[0].length];
 
@@ -15,79 +17,62 @@ public class Main {
 		for(int i=0; i<matrix.length; i++){
 			for(int j=0; j<matrix[0].length; j++){
 				if(set[i][j]){
-					int count = createGroups(matrix, set, i, j);
+					int count = 0;
+					int finalCount = createGroups(matrix, set, i, j, count, numRows, numCols);
 
 					//Insert into list
-					System.out.println(matrix[i][j] + " " + count);
-
+					System.out.println(matrix[i][j] + " " + finalCount);
 				}
 			}
 		}
-
-
 		//return groupSizes;
-
+	}
+	
+	public int getval(int [][] matrix, int row, int column, int numRows, int numCols){
+	
+		if(row >= 0 && row < numRows && column >= 0 && column < numCols){
+			return matrix[row][column];
+		}
+		
+		else{
+			return Integer.MIN_VALUE;
+		}
 	}
 
-	public int createGroups(int [][] matrix, boolean [][] set, int row, int column){
-
-		int count = 0;	
-		int numRows = matrix.length;
-		int numCols = matrix[0].length;
-		int self = matrix[row][column];
-		int side = -1; 
-		int below = -1;
-
-		//Normal case
-		if(row < numRows - 1 && column < numCols - 1){
-			side = matrix[row][column+1];
-			below = matrix[row+1][column];
+	public int createGroups(int [][] matrix, boolean [][] set, int row, int column, int count, int numRows, int numCols){
+		
+		if(row >= numRows || column >= numCols){
+			return Integer.MIN_VALUE;
 		}
-
-		//Edge case for last row
-		else if(row == numRows - 1 && column != numCols - 1){
-			side = matrix[row][column+1];
+		
+		count++;
+		set[row][column] = false;
+		int direction [][] = {{1,0}, {0,1}, {-1,0}, {0,-1}};
+		
+		for(int i=0; i<4; i++){
+			int newi = row + direction[i][0];
+			int newj = column + direction[i][1];
+			
+			int val = getval(matrix, newi, newj, numRows, numCols);
+			
+			if(val == matrix[row][column] && set[newi][newj]){
+				//System.out.println(newi+" "+newj);
+				count=createGroups(matrix, set, newi, newj,count, numRows, numCols);
+			}
 		}
-
-		//Edge case for last column
-		else if(column == numCols - 1 && row != numRows - 1){
-			below = matrix[row+1][column];
-		}
-
-		//Base case + edge case
-		if((self != side && self != below) || (row == numRows-1 && column == numCols-1)){
-			return count+1;
-		}
-
-		else if(column < numCols-1 && self == side && self != below){
-			set[row][column+1] = false;
-			return createGroups(matrix, set, row, column+1)+1;
-		}
-
-		else if(row < numRows-1 && self == below && self != side){
-			set[row+1][column] = false;
-			return createGroups(matrix, set, row+1, column)+1;
-		}
-
-		else if(self == side && self == below){
-			set[row][column+1] = false;
-			set[row+1][column] = false;
-			return createGroups(matrix, set, row, column+1) + createGroups(matrix, set, row+1, column) + 2;
-		}
-
-		return 0;
-
+		
+		return count;
 	}
-
+	
 	public static void main(String[] args) {
 
 		Main obj = new Main();
 
 		int [][] matrix = new int[][]{
-				{0,1,1,3},
-				{1,0,1,2},
-				{1,0,0,2},
-				{1,1,1,1}
+				{0,0,0,0},
+				{0,0,1,0},
+				{0,0,0,1},
+				{0,0,0,0}
 		};
 		
 		obj.grouping(matrix);
